cce7b8e52ccc4977e12330afe2b8099961ceeb32
mildmojo <mildmojo@users.noreply.github.com>
2014-03-05 03:57:56 -0500

    Refactors coin_slot.ino. Windows instructions:
    Setting up the Arduino IDE for the Trinket in Windows was kind of
    hellish.
    
    - Download the Arduino IDE v1.05 & install.
    - Follow Adafruit's "slow way" instructions for setting up the
      Trinket with the Arduino IDE.
      (http://learn.adafruit.com/introducing-trinket/setting-up-with-arduino-ide#the-slow-way)
    - Download Adafruit's Windows driver for the USBtinyISP
      (http://learn.adafruit.com/usbtinyisp/download).
    - In Device Manager, right-click the Trinket, update driver, and
      point to the place where you unzipped the Windows driver.
    - Grab the Bounce2 and TrinketKeyboard libraries and put them in
      folders in C:\Users\<user>\Documents\Arduino\libraries.
      (https://github.com/thomasfredericks/Bounce-Arduino-Wiring
       https://github.com/adafruit/Adafruit-Trinket-USB)
    - Start the Arduino IDE.
    - Make sure Tools => Board is set to "Adafruit Trinket 16MHz".
    - Make sure Tools => Programmer is set to "USBtinyISP".
    - Verify (compile) the sketch.
    - Press the Trinket's reset button.
    - While the Trinket is flashing red, hit the Arduino IDE upload button.
    
    Hopefully that all works.

--- firmware/coin_slot/coin_slot.ino
+++ firmware/coin_slot/coin_slot.ino
@@ -1,31 +1,41 @@
 /*
- * lex_coin_key.ino
+ * coin_slot.ino
  *
- * One-key USB keyboard.
+ * One-key USB keyboard using the Adafruit Trinket microcontroller.
  *
- * Designed to report key insertion events on Run Jump Dev's Lexitron
- * arcade cabinet.
+ * Designed to report coin insertion events on Run Jump Dev's Lexitron
+ * arcade cabinet. Sends a '=' keydown/keyup each time the coin switch
+ * is tripped. Wire SWITCH_PIN -> switch -> ground.
+ *
+ * Part of the Lexitron project:
+ *   https://github.com/runjumpdev/lexitron
  *
  * Adafruit Trinket settings:
  *   Board Type: Adafruit Trinket 16MHz
  *   Programmer: USBtinyISP
- *   
+ *
+ * Dependencies:
+ *   Bounce2: https://github.com/thomasfredericks/Bounce-Arduino-Wiring
+ *   TrinketKeyboard: https://github.com/adafruit/Adafruit-Trinket-USB
+ *
  */
 
-#include <Bounce.h>
+#include <Bounce2.h>
 #include <TrinketKeyboard.h>
 
 // Magic numbers
-#define LED_PIN     1
 #define SWITCH_PIN  0
+#define LED_PIN     1
 #define DEBOUNCE_MS 10
-#define COIN_KEY    KEYCODE_EQUAL
+#define COIN_KEY    KEYCODE_EQUAL // '=' key
 
 // Globals
-Bounce coin_switch = Bounce(SWITCH_PIN, DEBOUNCE_MS);
+Bounce coin_switch = Bounce();
 
 void setup() {
   pinMode(SWITCH_PIN, INPUT_PULLUP);
+  coin_switch.attach(SWITCH_PIN);
+  coin_switch.interval(DEBOUNCE_MS);
   TrinketKeyboard.begin();
 }
 
@@ -33,15 +43,24 @@ void loop() {
   TrinketKeyboard.poll();
 
   if (coin_switch.update()) {
-    if (coin_switch.fallingEdge()) {
-      //TrinketKeyboard.typeChar(COIN_KEY);
-      TrinketKeyboard.pressKey(0, COIN_KEY);
-      led_on();
-    } else if (coin_switch.risingEdge()) {
-      TrinketKeyboard.pressKey(0, 0);
+    // Switch pin goes high to low when closed.
+    if (coin_switch.read()) {
+      key_up();
       led_off();
+    } else {
+      key_down();
+      led_on();
     }
   }
+}
+
+void key_down() {
+  //TrinketKeyboard.typeChar(COIN_KEY);
+  TrinketKeyboard.pressKey(0, COIN_KEY);
+}
+
+void key_up() {
+  TrinketKeyboard.pressKey(0, 0);
 }
 
 void led_on() {
@@ -51,4 +70,3 @@ void led_on() {
 void led_off() {
   digitalWrite(LED_PIN, LOW);
 }
-
